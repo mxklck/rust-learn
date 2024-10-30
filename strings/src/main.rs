a8c99885ae18afbaf3bbf5e8b98566cfed7e96c6
Max Kloucek <maximiliankloucek@gmail.com>
2024-10-30 18:18:03 +0000

    commit strings again

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = "initial string".to_string();
+    // can concat strings with the + operator
+    println!("{s}");
+    s += " is now longer!";
+    println!("{s}");
+    s.push('!'); // adds a single character
+    println!("{s}");
+    
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    println!("{s3}");
+    // I don't really care about strings... skipping this!
 }
