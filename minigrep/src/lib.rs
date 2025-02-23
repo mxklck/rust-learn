a6245a2c7cb0c2f681c6df3f8b9dfaec1c95bb21
mxklck <maximiliankloucek@gmail.com>
2025-02-23 17:07:06 +0000

    Finish minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,1 +1,110 @@
+use std::env;
+use std::error::Error;
+use std::fs;
 
+// everything is public here
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    // new is expected to never fail, so we rename to build
+    // 'static is a lifetime specifier,
+    // which means that the reference will live for the entire duration of the program
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments provided");
+        }
+
+        // clone is used to create a new string from the reference
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Self {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.file_path)?;
+
+    // I hate this syntax :(
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in result {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+// lifetime specifier 'a denotes that the returned vector string slices will
+// live at least as long as the contents string
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // lines() method returns an iterator
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+// not borrowed mutably because we don't want to modify the query
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase(); // we shadow the original query
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            // reference required as contains expects a string slice
+            results.push(line); // note we push the original line, not the lowercase one
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // this imports all the public functions from the parent module
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
